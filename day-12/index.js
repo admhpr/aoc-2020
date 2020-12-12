a76bc5da69b92a43eda53ccd0efdab6c5c878320
adam <adam@harpur.io>
2020-12-12 14:06:46 +0000

    day 12

---
+++ day-12/index.js
@@ -0,0 +1,119 @@
+const { readFileSync } = require("fs")
+const inputs = readFileSync("./input", "UTF-8").split(/\n/)
+function rotate(rotation, degrees, ship) {
+  let turn = degrees
+  const rotations = {
+    L: { E: "N", N: "W", W: "S", S: "E" },
+    R: { E: "S", S: "W", W: "N", N: "E" },
+  }
+  while (turn > 0) {
+    ship.direction = rotations[rotation][ship.direction]
+    turn -= 90
+  }
+}
+function getDistanceTravelled(inputs) {
+  const ship = {
+    x: 0,
+    y: 0,
+    direction: "E",
+  }
+  for (const instruction of inputs) {
+    let action = [...instruction][0]
+    const amount = parseInt([...instruction].slice(1).join(""))
+    if (action === "N") {
+      ship.y -= amount
+    }
+    if (action === "S") {
+      ship.y += amount
+    }
+    if (action === "E") {
+      ship.x += amount
+    }
+    if (action === "W") {
+      ship.x -= amount
+    }
+    if (action === "L") {
+      rotate(action, amount, ship)
+    }
+    if (action === "R") {
+      rotate(action, amount, ship)
+    }
+    if (action === "F") {
+      switch (ship.direction) {
+        case "E":
+          ship.x += amount
+          break
+        case "W":
+          ship.x -= amount
+          break
+        case "N":
+          ship.y -= amount
+          break
+        case "S":
+          ship.y += amount
+          break
+      }
+    }
+  }
+  return Math.abs(ship.x) + Math.abs(ship.y)
+}
+function rotateWaypoints(rotation, degrees, ship) {
+  let turn = degrees
+  while (turn > 0) {
+    if (rotation === "R") {
+      ship.waypoint = { x: -ship.waypoint.y, y: ship.waypoint.x }
+    }
+    if (rotation === "L") {
+      ship.waypoint = { x: ship.waypoint.y, y: -ship.waypoint.x }
+    }
+    turn -= 90
+  }
+}
+function getDistanceTravelledWithWaypoints(inputs) {
+  const ship = {
+    x: 0,
+    y: 0,
+    waypoint: {
+      x: 10,
+      y: -1,
+    },
+    direction: "E",
+  }
+  for (const instruction of inputs) {
+    let action = [...instruction][0]
+    const amount = parseInt([...instruction].slice(1).join(""))
+    if (action === "N") {
+      ship.waypoint.y -= amount
+    }
+    if (action === "S") {
+      ship.waypoint.y += amount
+    }
+    if (action === "E") {
+      ship.waypoint.x += amount
+    }
+    if (action === "W") {
+      ship.waypoint.x -= amount
+    }
+    if (action === "L") {
+      rotateWaypoints(action, amount, ship)
+    }
+    if (action === "R") {
+      rotateWaypoints(action, amount, ship)
+    }
+
+    if (action === "F") {
+      ship.y += amount * ship.waypoint.y
+      ship.x += amount * ship.waypoint.x
+    }
+  }
+  return Math.abs(ship.x) + Math.abs(ship.y)
+}
+function main(inputs) {
+  const distanceTravelled = getDistanceTravelled(inputs)
+  const distanceTravelledWithWaypoints = getDistanceTravelledWithWaypoints(
+    inputs
+  )
+  console.log("distance", distanceTravelled)
+  console.log("distance with waypoints", distanceTravelledWithWaypoints)
+}
+main(inputs)
